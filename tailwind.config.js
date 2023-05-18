75e499584a9f2d2d2150db92dd8bcb4f5e962576
Ravi <rg.opash@gmail.com>
2023-05-18 16:53:58 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-      extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
-  plugins: [
-      require('tw-elements/dist/plugin')
-  ], }
+  plugins: [],
+}
